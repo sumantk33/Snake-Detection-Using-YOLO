83e6b807f0b4fb496039a27f0c7652e453e17422
sumantk33 <65009624+sumantk33@users.noreply.github.com>
2020-05-30 21:19:22 +0530

    Add files via upload

---
+++ yolo_snake_detection.py
@@ -0,0 +1,74 @@
+import cv2
+import numpy as np
+import glob
+import random
+import time
+
+def detect_snake(cap):
+    net = cv2.dnn.readNet("yolov3_training_last (1).weights", "yolov3_testing.cfg")
+
+    classes = ["snake"]
+
+    layer_names = net.getLayerNames()
+    output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+    colors = np.random.uniform(0, 255, size=(len(classes), 3))
+    font = cv2.FONT_HERSHEY_PLAIN
+
+    start = time.time()
+    frame_id = 0
+
+    while True:
+        _, frame = cap.read()
+        height, width, channels = frame.shape
+        frame_id += 1
+
+        blob = cv2.dnn.blobFromImage(frame, 0.00392, (320, 320), (0, 0, 0), True, crop=False)
+
+        net.setInput(blob)
+        outs = net.forward(output_layers)
+
+        class_ids = []
+        confidences = []
+        boxes = []
+        for out in outs:
+            for detection in out:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+                if confidence > 0.3:
+                    #Snake detected
+                    #print(class_id)
+                    center_x = int(detection[0] * width)
+                    center_y = int(detection[1] * height)
+                    w = int(detection[2] * width)
+                    h = int(detection[3] * height)
+
+                    x = int(center_x - w / 2)
+                    y = int(center_y - h / 2)
+
+                    boxes.append([x, y, w, h])
+                    confidences.append(float(confidence))
+                    class_ids.append(class_id)
+
+        indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+        #print(indexes)
+        
+        for i in range(len(boxes)):
+            if i in indexes:
+                x, y, w, h = boxes[i]
+                label = str(classes[class_ids[i]])
+                color = colors[class_ids[i]]
+                color = (0,0,255)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+                cv2.putText(frame, label, (x, y + 30), font, 3, color, 2)
+                
+        elapsed_time = time.time() - start
+        fps = elapsed_time/frame_id
+        cv2.putText(frame, "FPS: " + str(round(fps, 2)), (10,30), font, 2, (0, 255, 0), 1)
+        cv2.imshow("Image", frame)
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
